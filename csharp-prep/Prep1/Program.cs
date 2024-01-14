80a72d5d2f5c600ce2e50e5314a6316aa60cb7cc
Jacob-Packer <pac20007@byui.edu>
2024-01-13 23:13:22 -0700

    a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        // Ask user for names
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Print name
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get percentage from user and convert to int
+        Console.WriteLine("Please enter a grade percentage: ");
+        string textFromUser = Console.ReadLine();
+        int percentage = int.Parse(textFromUser);
+
+        // compare percentage on if tree to determine if letter grade changes
+        string letterGrade = "A";
+        if (percentage < 90)
+            letterGrade = "B";
+        else if (percentage < 80)
+            letterGrade = "C";
+        else if (percentage < 70)
+            letterGrade = "D";
+        else if (percentage < 60)
+            letterGrade = "F";
+
+        // print letter grade
+        Console.WriteLine($"Your grade is a(n) {letterGrade}.");
     }
 }
